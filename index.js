cd94f8805f313d20d94a3e2a1bacb4def606a2b8
Zusier <46111482+Zusier@users.noreply.github.com>
2020-07-29 16:56:45 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+const axios = require('axios').default;
+
+const client = new Discord.Client();
+
+const {
+    account_token
+} = require("./config.json")
+
+client.on('ready', () => {
+    console.log("Looking for Nitros");
+});
+
+client.on('message', message => {
+    if(message.content.includes('discord.gift') || message.content.includes('discordapp.com/gifts/')) {
+
+        var Nitro = /(discord\.(gift)|discordapp\.com\/gift)\/.+[a-z]/
+
+        var NitroUrl = Nitro.exec(message.content);
+        var NitroCode = NitroUrl[0].split('/')[1];
+
+        console.log(`A Nitro was found in ${message.guild.name}`);
+
+        axios({
+            method: 'POST',
+            url: `https://discordapp.com/api/v6/entitlements/gift-codes/${NitroCode}/redeem`,
+            headers:
+            {
+                'Authorization': client.account_token
+            }
+        }).then(
+            () => console.log(`Successfully redeemed a nitro that was found in ${message.guild.name}`)
+
+        ).catch(ex => console.log(`Couldn't claim Nitro. Link is expired or it's already claimed!`))
+
+    }
+})
+
+client.login(account_token)
